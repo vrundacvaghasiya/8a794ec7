3d5281482df0256e97d8d5529b76b45b9391a093
vrundacvaghasiya <vrundacvaghasiya@gmail.com>
2025-03-09 13:40:58 -0400

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,46 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path=require("path")
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
+  entry: './src/index.js', // Your main JS entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
-  performance: { hints: false },
   module: {
     rules: [
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
+  performance: {
+    maxEntrypointSize: 600000, // Set a higher limit (e.g., 1MB) for the entry point
+    maxAssetSize: 600000, // Set a higher limit (e.g., 1MB) for assets
+    hints: "warning", // You can set this to 'error' or 'none' if you prefer no warnings or errors
+  },
+
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "index.html",
+    }),
+  ],
 };
